9a87de6f3bce330b0640c2e4c11f20840ef69073
IrinaMironov <irina.mironov1@gmail.com>
2018-05-02 19:13:27 +0300

    Revert "Add files via upload"
    This reverts commit f62b40c9c2a2493d7edeff1de5367e3331d20461.

--- code.h
+++
@@ -1,8 +0,0 @@
-#ifndef CODE_H_
-#define CODE_H_
-
-
-void codificare(char mesaj_fcc[250], char tastatura_fcc[], char codificat_fcc[250]);
-void decodificare(char mesaj_fc[250], char tastatura_fc[], char decodificat_fc[250]);
-
-#endif
